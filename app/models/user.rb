82e6b3b24ca07753781562f6605ec1d2ea7ed676
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-25 09:58:07 +0000

    finish

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  # belongs_to :book
+  has_many :books
+  validates :name, length: { minimum: 2, maximum: 20 }, presence: true
+  validates :introduction, presence: true
+
+
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
