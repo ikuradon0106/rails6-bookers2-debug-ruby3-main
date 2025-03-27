0c6d796b3e27f36477f05b027fa9a491a3b3d39b
EC2 Default User <ec2-user@ip-172-31-40-77.ap-northeast-1.compute.internal>
2025-03-27 02:54:11 +0000

    [next]コメント機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,33 @@
 class FavoritesController < ApplicationController
   def create
     # いいねを作成する
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to book_path(book)
+    redirect_back fallback_location: root_path
+    
+    # if request.referer == book_url(@book) #詳細画面にいいねを押した場合
+      #redirect_to book_path(@book)
+    #else
+     # redirect_to books_path  # それ以外の場合は一覧画面にリダイレクト
+    # end
+
   end
 
   def destroy
     # いいねを削除する
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to book_path(book)
-  end
+    redirect_back fallback_location: root_path
 
+
+    #if request.referer == book_url(@book) #詳細画面にいいねを押した場合
+      #redirect_to book_path(@book)
+    #else
+      #redirect_to books_path  # それ以外の場合は一覧画面にリダイレクト
+    
+    #end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorite, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
-  resource :favorite, only: [:create, :destroy]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
